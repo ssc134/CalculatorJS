85b4e0011031bdf75fb83f3133adf39af3f76758
Saurabh Chavan <31591900+ssc134@users.noreply.github.com>
2019-10-23 20:43:06 +0530

    Add files via upload
    v1.0

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+function calc()
+{
+    var a = document.getElementById("operand1").value;
+    a = Number.parseFloat(a);
+    var b = document.getElementById("operand2").value;
+    b = Number.parseFloat(b);
+    var op = document.getElementById('operator').value;
+    var res;
+    if(op=='+')
+        res = a+b;
+    else if(op=='-')
+        res = a-b;
+    else if(op=='*')
+        res = a*b;
+    else if(op=='/')
+        res = a/b;
 
+    document.getElementById("result").value = res;
+    console.log(`${res}`);
+    
+}
+
+function copy1()
+{
+    document.getElementById('operand1').value = document.getElementById('result').value;
+}
+
+function copy2()
+{
+    document.getElementById("operand2").value = document.getElementById('result').value;
+}
