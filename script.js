4418d4e4a43033249229a1dd2d4c857689b47c30
Saurabh Chavan <31591900+ssc134@users.noreply.github.com>
2019-10-31 13:13:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,98 @@
-function calc()
-{
-    var a = document.getElementById("operand1").value;
-    a = Number.parseFloat(a);
-    var b = document.getElementById("operand2").value;
-    b = Number.parseFloat(b);
-    var op = document.getElementById('operator').value;
-    var res;
-    if(op=='+')
-        res = a+b;
-    else if(op=='-')
-        res = a-b;
-    else if(op=='*')
-        res = a*b;
-    else if(op=='/')
-        res = a/b;
+var text = document.getElementById("inputText");
+var buffer = null;
+var operator = null;
 
-    document.getElementById("result").value = res;
-    console.log(`${res}`);
+function initialize()
+{
+    console.log(`text = ${text}`) ;
+    console.log(`buffer = ${buffer}`) ;
+    console.log(`operator = ${operator}`) ;
+}
+
+function onClick1()
+{
+    console.log("1 Clicked");
     
 }
 
-function copy1()
+function onClick2()
 {
-    document.getElementById('operand1').value = document.getElementById('result').value;
+    console.log("2 Clicked");
 }
 
-function copy2()
+function onClick3()
 {
-    document.getElementById("operand2").value = document.getElementById('result').value;
+    console.log("3 Clicked");
 }
+
+function onClick4()
+{
+    console.log("4 Clicked");
+}
+
+function onClick5()
+{
+    console.log("5 Clicked");
+}
+
+function onClick6()
+{
+    console.log("6 Clicked");
+}
+
+function onClick7()
+{
+    console.log("7 Clicked");
+}
+
+function onClick8()
+{
+    console.log("8 Clicked");
+}
+
+function onClick9()
+{
+    console.log("9 Clicked");
+}
+
+function onClick0()
+{
+    console.log("0 Clicked");
+}
+
+function onClickPlus()
+{
+    console.log("+ Clicked");
+}
+
+function onClickMinus()
+{
+    console.log("- Clicked");
+}
+
+function onClickDivide()
+{
+    console.log("/ Clicked");
+}
+
+function onClickMultiply()
+{
+    console.log("* Clicked");
+}
+
+function onClickDot()
+{
+    console.log(". Clicked");
+    
+}
+
+function onClickClear()
+{
+    console.log("AC Clicked");
+}
+
+function onClickEvaluate()
+{
+    console.log("= Clicked");
+}
+
