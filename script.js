d0bbdfebe95e3cef7b85f471ed0eb7698d057ea2
Saurabh Chavan <31591900+ssc134@users.noreply.github.com>
2019-10-31 14:51:04 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -15,70 +15,80 @@ function onClick1()
 {
     console.log("1 Clicked");
     text += "1";
-    document.getElementById("inputText").value = text;
+//    document.getElementById("inputText").value = text;
+    input.value = text;
 }
 
 function onClick2()
 {
     console.log("2 Clicked");
     text += "2";
-    document.getElementById("inputText").value = text;
+//    document.getElementById("inputText").value = text;
+    input.value = text;
 }
 
 function onClick3()
 {
     console.log("3 Clicked");
     text += "3";
-    document.getElementById("inputText").value = text;
+//    document.getElementById("inputText").value = text;
+    input.value = text;
 }
 
 function onClick4()
 {
     console.log("4 Clicked");
     text += "4";
-    document.getElementById("inputText").value = text;
+//    document.getElementById("inputText").value = text;
+    input.value = text;
 }
 
 function onClick5()
 {
     console.log("5 Clicked");
     text += "5";
-    document.getElementById("inputText").value = text;
+//    document.getElementById("inputText").value = text;
+    input.value = text;
 }
 
 function onClick6()
 {
     console.log("6 Clicked");
     text += "6";
-    document.getElementById("inputText").value = text;
+//    document.getElementById("inputText").value = text;
+    input.value = text;
 }
 
 function onClick7()
 {
     console.log("7 Clicked");
     text += "7";
-    document.getElementById("inputText").value = text;
+//    document.getElementById("inputText").value = text;
+    input.value = text;
 }
 
 function onClick8()
 {
     console.log("8 Clicked");
     text += "8";
-    document.getElementById("inputText").value = text;
+//    document.getElementById("inputText").value = text;
+    input.value = text;
 }
 
 function onClick9()
 {
     console.log("9 Clicked");
     text += "9";
-    document.getElementById("inputText").value = text;
+//    document.getElementById("inputText").value = text;
+    input.value = text;
 }
 
 function onClick0()
 {
     console.log("0 Clicked");
     text += "0";
-    document.getElementById("inputText").value = text;
+//    document.getElementById("inputText").value = text;
+    input.value = text;
 }
 
 function onClickPlus()
@@ -121,14 +131,19 @@ function onClickMultiply()
 function onClickDot()
 {
     console.log(". Clicked");
-    
+    if(text.indexOf('.')==-1)
+    {
+        text += '.';
+        input.value = text;
+    }
 }
 
 function onClickClear()
 {
     console.log("AC Clicked");
     text = "";
-    document.getElementById("inputText").value = text;
+//    document.getElementById("inputText").value = text;
+    input.value = text;
 }
 
 function onClickEvaluate()
